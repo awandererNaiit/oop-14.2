a6175ae733d4a68f57d3d7ee6bbe13f1dd7d2cef
Maksim Bodnar <maximsevsk@gmail.com>
2024-04-25 14:18:31 +0300

    mb like this

---
+++ main.py
@@ -0,0 +1,111 @@
+class Category:
+    total_categories = 0
+    total_unique_products = 0
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self._products = []
+        Category.total_categories += 1
+
+    @property
+    def products(self):
+        return self._products
+
+    @products.setter
+    def products(self, products):
+        self._products = products
+
+    def add_product(self, product):
+        if not isinstance(product, Product):
+            raise TypeError("Нельзя добавлять разные продукты")
+        self._products.append(product)
+        Category.total_unique_products += 1
+
+    def remove_product(self, product):
+        self._products.remove(product)
+        Category.total_unique_products -= 1
+
+    def get_products_info(self):
+        products_info = []
+        for product in self._products:
+            product_info = f"{product.name}, {product.price} руб. Остаток: {product.amount} шт."
+            products_info.append(product_info)
+        return products_info
+
+    def __str__(self):
+        total_products = len(self)
+        return f"{self.name}, количество продуктов: {total_products} шт."
+
+    def __len__(self):
+        return sum(product.amount for product in self._products)
+
+
+class Product:
+    def __init__(self, name, description, price, amount):
+        self._name = name
+        self._description = description
+        self._price = price
+        self._amount = amount
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @description.setter
+    def description(self, value):
+        self._description = value
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = value
+
+    @staticmethod
+    def check_type(obj1, obj2):
+        if not isinstance(obj1, Product) or not isinstance(obj2, Product):
+            raise TypeError("Нельзя добавлять разные продукты")
+
+    def __add__(self, other):
+        self.check_type(self, other)
+        return self.price * self.amount + other.price * other.amount
+
+    @property
+    def total_price(self):
+        return self.price * self.amount
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, amount, brand, model, storage, color):
+        super().__init__(name, description, price, amount)
+        self._brand = brand
+        self._model = model
+        self._storage = storage
+        self._color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, amount, origin, duration, color):
+        super().__init__(name, description, price, amount)
+        self._origin = origin
+        self._duration = duration
+        self._color = color
